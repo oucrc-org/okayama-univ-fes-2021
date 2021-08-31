51905713cb8760fb562b447daaee92d7ec9d7677
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-08-31 19:13:25 +0900

    Add: layoutsフォルダを追加

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Navigation />
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Footer from '@/components/Footer.vue'
+import Navigation from '@/components/Navigation.vue'
+
+export default Vue.extend({
+  components: {
+    Navigation,
+    Footer
+  }
+})
+</script>
