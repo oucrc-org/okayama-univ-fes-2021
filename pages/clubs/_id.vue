0bafa9ff71c3bf14e5f063b1645d73a25ee1fe81
Ryo Ando <sasigume@gmail.com>
2021-09-02 01:33:34 +0900

    部活の404がVercelでハンドリングできていなかったので修正

--- pages/clubs/_id.vue
+++ pages/clubs/_id.vue
@@ -9,9 +9,11 @@ import { Context } from '@nuxt/types'
 
 export default {
   async asyncData (context: Context) {
-    const { app, params } = context
+    const { app, params, error } = context
     const response = await app.$microcms.get({
       endpoint: `clubs/${params.id}`
+    }).catch(() => {
+      error({ statusCode: 404, message: 'お探しの部活動・サークルが見つかりませんでした。' })
     })
     return {
       club: response
