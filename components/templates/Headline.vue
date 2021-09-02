a819dd014eddc1bc0c8970d32a82230e44d0ad32
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-09-03 00:03:29 +0900

    [add] responseを入れるクラスを作成

---
+++ components/templates/Headline.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="text-3xl border-b-4 mt-16 pb-3 w-4/5 sm:w-2/3 relative z-10" :class="colors">
+    {{ title }}
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    colors: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
--- pages/clubs/_id.vue
+++ pages/clubs/_id.vue
@@ -4,37 +4,50 @@
       <Header :title="club.name" colors="bg-themeColor text-white" />
       <img :src="club.cover.url" alt="">
     </div>
-    <h1>Hello</h1>
+    <!--  -->
+    <Headline title="何をしている部活なの？" colors="border-themeColor text-themeColor" />
     <div>
       <HTMLLeader :body="club.body" />
     </div>
+    <!-- -->
+    <Headline title="活動紹介映像" colors="border-themeColor text-themeColor" />
+    <div>
+      <iframe :src="watchToEmbed(club.youtubeUrl)" />
+    </div>
+    <!-- -->
+    <Headline title="公式SNS" colors="border-themeColor text-themeColor" />
   </div>
 </template>
 
 <script lang="ts">
 import { Context } from '@nuxt/types'
-import Header from '~/components/templates/Header.vue'
+import Header from '~/components/layouts/Header.vue'
 import HTMLLeader from '~/components/templates/HTMLLeader.vue'
+import Headline from '~/components/templates/Headline.vue'
+import watchToEmbed from '~/assets/js/url/watch-to-embed'
+import Club from '~/assets/js/type/club/club'
 
 export default {
-  components: { HTMLLeader, Header },
+  components: { Headline, HTMLLeader, Header },
   async asyncData (context: Context) {
     const { app, params } = context
     const response = await app.$microcms.get({
       endpoint: `clubs/${params.id}`
     })
     return {
-      club: response
+      club: new Club(response)
     }
   },
   data () {
     return {
       club: {
-        name: '',
-        cover: '',
-        body: ''
+        type: Club,
+        default: Club
       }
     }
+  },
+  methods: {
+    watchToEmbed
   }
 }
 </script>
