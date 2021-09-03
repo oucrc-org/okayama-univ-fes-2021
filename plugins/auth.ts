b443276416f31942eb459da13a0ad05e173dea69
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-09-03 10:32:14 +0900

    Add: Cloud Firestore アカウント登録機能

--- plugins/auth.ts
+++ plugins/auth.ts
@@ -1,4 +1,6 @@
+import firebaseApp from 'firebase'
 import firebase from '~/plugins/firebase'
+import FieldValue = firebaseApp.firestore.FieldValue
 
 export default ({
   app,
@@ -6,11 +8,33 @@ export default ({
   store
 }: { app: any, redirect: any, store: any }) => {
   app.router.afterEach((to: any) => {
-    firebase.auth().onAuthStateChanged((user) => {
-      store.commit('user/setUser', user)
+    firebase.auth().onAuthStateChanged(async (user) => {
+      if (user) {
+        const data = await firebase.firestore().collection('users').doc(user.uid).get()
+        if (data.exists) {
+          await firebase.firestore().collection('users').doc(user.uid).update({
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+            email: user.email,
+            emailVerified: user.emailVerified,
+            updatedAt: FieldValue.serverTimestamp()
+          })
+        } else {
+          await firebase.firestore().collection('users').doc(user.uid).set({
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+            email: user.email,
+            emailVerified: user.emailVerified,
+            createdAt: FieldValue.serverTimestamp(),
+            updatedAt: FieldValue.serverTimestamp()
+          })
+        }
 
-      if (user && to.name === 'login') {
-        redirect('/mypage')
+        store.commit('user/setUser', user)
+
+        if (to.name === 'login') {
+          redirect('/mypage')
+        }
       }
     })
   })
