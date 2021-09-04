eec8915914a3a4f7a3815104bfa7985c96d1de8a
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-09-04 22:20:27 +0900

    Fix: フッター Clubsで非表示

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="mb-8 mt-20">
-    <h2 class="text-center text-3xl text-gray-800 font-medium tracking-wider">
-      SHARE ON
-    </h2>
-    <div class="text-center w-full mt-14">
-      <a href="#" class="text-lg border border-gray-300 inline-block rounded-full py-5 w-5/6 sm:w-96">
-        <img class="inline-block text-gray-700 tracking-widest h-6 mr-5" src="@/assets/img/twitter.png" alt="Twitter">
-        Twitter で シェア
-      </a>
-    </div>
-    <div class="text-center w-full mt-5">
-      <a href="#" class="text-lg border border-gray-300 inline-block rounded-full py-5 w-5/6 sm:w-96">
-        <img class="inline-block text-gray-700 tracking-widest h-6 mr-5" src="@/assets/img/facebook.png" alt="Facebook">
-        Facebook で シェア
-      </a>
-    </div>
-    <div class="text-center w-full mt-5">
-      <a href="#" class="text-lg border border-gray-300 inline-block rounded-full py-5 w-5/6 sm:w-96">
-        <img class="inline-block text-gray-700 tracking-widest h-6 mr-5" src="@/assets/img/line.png" alt="LINE">
-        LINE で シェア
-      </a>
+    <div v-if="!$nuxt.$route.path.match('/clubs/*')">
+      <h2 class="text-center text-3xl text-gray-800 font-medium tracking-wider">
+        SHARE ON
+      </h2>
+      <div class="text-center w-full mt-14">
+        <a href="#" class="text-lg border border-gray-300 inline-block rounded-full py-5 w-5/6 sm:w-96">
+          <img class="inline-block text-gray-700 tracking-widest h-6 mr-5" src="@/assets/img/twitter.png" alt="Twitter">
+          Twitter で シェア
+        </a>
+      </div>
+      <div class="text-center w-full mt-5">
+        <a href="#" class="text-lg border border-gray-300 inline-block rounded-full py-5 w-5/6 sm:w-96">
+          <img
+            class="inline-block text-gray-700 tracking-widest h-6 mr-5"
+            src="@/assets/img/facebook.png"
+            alt="Facebook"
+          >
+          Facebook で シェア
+        </a>
+      </div>
+      <div class="text-center w-full mt-5">
+        <a href="#" class="text-lg border border-gray-300 inline-block rounded-full py-5 w-5/6 sm:w-96">
+          <img class="inline-block text-gray-700 tracking-widest h-6 mr-5" src="@/assets/img/line.png" alt="LINE">
+          LINE で シェア
+        </a>
+      </div>
     </div>
 
     <hr class="mt-16 mx-auto w-11/12">
@@ -32,7 +38,13 @@
 
 <script>
 export default {
-  name: 'Footer'
+  name: 'Footer',
+  props: {
+    hiddenShare: {
+      type: Boolean,
+      default: false
+    }
+  }
 }
 </script>
 
