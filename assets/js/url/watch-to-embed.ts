d169c1dab34e2b64f027061c73f6905d11b09368
AAAR-Salmon <tenzikujohn@gmail.com>
2021-09-04 21:25:42 +0900

    Fix: 活動紹介映像で、v以外のクエリパラメータが付いている場合に正しく動画IDが指定できていない問題を修正

--- assets/js/url/watch-to-embed.ts
+++ assets/js/url/watch-to-embed.ts
@@ -2,7 +2,34 @@
  * Youtubeのurlにおいて、閲覧用から埋め込み用に変換する関数
  * @param url
  */
-const watchToEmbed = (url?: String) => url?.replace(/youtu.be\/([^?&]+)/, 'www.youtube.com/embed/$1')
-  .replace(/\/watch\?v=([^&]+)/, '/embed/$1')
+// https://youtu.be/wNSN3fPGHsI?t=291
+const watchToEmbed = (url?: string) => {
+  if (typeof url === 'undefined') {
+    return null
+  }
+
+  if (url.includes('/embed')) {
+    return url
+  }
+
+  const videoId = url.match(/(?:[?&]v=|youtu\.be\/)(?<videoId>[^?&]+)/)?.groups?.videoId
+
+  if (!videoId) {
+    return null
+  }
+
+  const result = new URL(videoId, 'https://youtube.com/embed/')
+  const paramsOtherThanId = new URLSearchParams(new URL(url).searchParams)
+  console.log(paramsOtherThanId.toString())
+  paramsOtherThanId.delete('v')
+  // 通常の開始時間指定（t=xx）を埋め込み用（start=xx）に書き換え
+  const startTime = paramsOtherThanId.get('t')
+  if (startTime !== null) {
+    paramsOtherThanId.delete('t')
+    paramsOtherThanId.set('start', startTime)
+  }
+  result.search = paramsOtherThanId.toString()
+  return result.toString()
+}
 
 export default watchToEmbed
