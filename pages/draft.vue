08e149eae1e5e11a9462c15138c87eee46fa904b
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-09-05 12:52:01 +0900

    Add: MicroCMS 画面プレビュー機能

---
+++ pages/draft.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="container max-w-screen-xl mt-24 mx-auto relative">
+    <!-- ヘッダー -->
+    <div class="my-5">
+      <Header :title="club.name" colors="bg-themeColor text-white" />
+      <VerticalTitle text="ABOUT" colors="text-gray-200" />
+      <div class="container max-w-screen-lg mt-14 mx-auto">
+        <div class="w-4/5 tracking-widest leading-6 px-4">
+          <img class="rounded-xl" :src="coverUrl" alt="団体画像">
+        </div>
+      </div>
+    </div>
+
+    <!-- 何している部活なの？ -->
+    <HTMLLeader :body="club.body" class="px-6" />
+
+    <!-- 活動紹介映像 -->
+    <BodyWithHeader title="活動紹介映像">
+      <div :class="$style['introduction_club_movie']">
+        <iframe :src="watchToEmbed(club.youtube_url)" allowfullscreen />
+      </div>
+    </BodyWithHeader>
+
+    <!-- 公式SNS -->
+    <div v-if="hasSNSUrl">
+      <BodyWithHeader title="公式SNS">
+        <div class="px-6">
+          <div class="grid grid-cols-4">
+            <!-- Twitter -->
+            <div v-if="club.twitter_url != null" class="mx-auto border-solid border-2 p-4 rounded-full">
+              <a :href="club.twitter_url">
+                <img
+                  class="inline-block text-gray-700 tracking-widest h-6"
+                  src="@/assets/img/twitter.png"
+                  alt="Twitter"
+                >
+              </a>
+            </div>
+
+            <!-- Facebook -->
+            <div v-if="club.facebook_url != null" class="mx-auto border-solid border-2 p-4 rounded-full">
+              <a :href="club.facebook_url">
+                <img
+                  class="inline-block text-gray-700 tracking-widest h-6"
+                  src="@/assets/img/facebook.png"
+                  alt="Facebook"
+                >
+              </a>
+            </div>
+
+            <!-- Instagram -->
+            <div v-if="club.instagram_url != null" class="mx-auto border-solid border-2 p-4 rounded-full">
+              <a :href="club.instagram_url">
+                <img class="inline-block text-gray-700 tracking-widest h-6" src="@/assets/img/instagram.png" alt="LINE">
+              </a>
+            </div>
+
+            <!-- LINE -->
+            <div v-if="club.line_url != null" class="mx-auto border-solid border-2 p-4 rounded-full">
+              <a :href="club.line_url">
+                <img class="inline-block text-gray-700 tracking-widest h-6" src="@/assets/img/line.png" alt="LINE">
+              </a>
+            </div>
+          </div>
+        </div>
+      </BodyWithHeader>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Header from '~/components/Header.vue'
+import HTMLLeader from '~/components/templates/HTMLLeader.vue'
+import watchToEmbed from '~/assets/js/url/watch-to-embed'
+import { ClubData } from '~/assets/js/type/club/club-data'
+import Club from '~/assets/js/type/club/club'
+import BodyWithHeader from '~/components/pages/club/_id/BodyWithHeader.vue'
+import VerticalTitle from '~/components/VerticalTitle.vue'
+
+export default Vue.extend({
+  components: {
+    BodyWithHeader,
+    HTMLLeader,
+    Header,
+    VerticalTitle
+  },
+  data (): {
+    club: ClubData
+    } {
+    return {
+      club: new Club()
+    }
+  },
+  computed: {
+    hasSNSUrl (): boolean {
+      // @ts-ignore
+      const club = this.club
+      return !(
+        club.twitter_url == null &&
+        club.facebook_url == null &&
+        club.instagram_url == null &&
+        club.line_url == null
+      )
+    },
+    coverUrl (): String {
+      // @ts-ignore
+      return this.club.cover ? this.club.cover.url : null
+    }
+  },
+  async created () {
+    const query = this.$route.query
+    if (!query.id || !query.draftKey) {
+      return
+    }
+    const { data }: { data: ClubData } = await this.$axios.get(
+      `https://oufes2021.microcms.io/api/v1/clubs/${query.id}?draftKey=${query.draftKey}`,
+      {
+        headers: { 'X-API-KEY': process.env.API_KEY }
+      }
+    )
+    this.club = data
+  },
+  methods: {
+    watchToEmbed
+  }
+})
+</script>
+
+<style module>
+.introduction_club_movie {
+  position: relative;
+  width: 100%;
+  height: 0;
+  padding-top: 75%;
+}
+
+.introduction_club_movie iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+h1 {
+  @apply border-themeColor text-themeColor text-xl px-4 pb-1 border-b-4 mb-6 mt-12 w-5/6 sm:w-2/3 relative -left-6 z-10
+}
+
+h2, h3, h4, h5 {
+  @apply mb-4 mt-8
+}
+
+</style>
