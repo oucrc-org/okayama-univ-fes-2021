7e385419b077eb7c471bb8ba772c5c327d22a74c
sasigume <sasigume@gmail.com>
2021-10-05 21:18:19 +0900

    Change: 暫定ログインボタンをデザイン

--- pages/login.vue
+++ pages/login.vue
@@ -1,34 +1,44 @@
 <template>
-  <div v-if="!isLoading">
-    <button v-if="!isLoading" @click="signInWithGoogleRedirect">
-      ログイン
-    </button>
+  <div
+    class="py-8 w-screen flex flex-col justify-center items-center"
+    v-if="!isLoading"
+  >
+    <a
+      id="button-login-google"
+      class="cursor-pointer inline-block mx-auto"
+      v-if="!isLoading"
+      @click="signInWithGoogleRedirect"
+    >
+      <img
+        src="@/assets/img/button_login_google.png"
+        width="206px"
+        height="55px"
+      />
+    </a>
   </div>
 </template>
 
 <script>
-import firebase, { googleProvider } from '~/plugins/firebase'
+import firebase, { googleProvider } from "~/plugins/firebase";
 
 export default {
-  name: 'Login',
-  middleware: 'skipLogin',
-  data () {
+  name: "Login",
+  middleware: "skipLogin",
+  data() {
     return {
       isLoading: true
-    }
+    };
   },
-  mounted () {
-    this.isLoading = false
+  mounted() {
+    this.isLoading = false;
   },
   methods: {
-    signInWithGoogleRedirect () {
-      firebase.auth().signInWithRedirect(googleProvider)
-      this.isLoading = true
+    signInWithGoogleRedirect() {
+      firebase.auth().signInWithRedirect(googleProvider);
+      this.isLoading = true;
     }
   }
-}
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
