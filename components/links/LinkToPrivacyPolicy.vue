af2f7b3fbbf1f1e5cb67a8a6d8096a4c37706753
sasigume <sasigume@gmail.com>
2021-10-05 23:17:48 +0900

    プライバシーポリシー用リンクを追加

---
+++ components/links/LinkToPrivacyPolicy.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- リンクが変わった際の修正を楽にするため、インポートは面倒ですがコンポーネントにしています -->
+  <NuxtLink class="text-xs text-gray-700" to="#">
+    プライバシーポリシー
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  name: 'LinkToPrivacyPolicy'
+}
+</script>
