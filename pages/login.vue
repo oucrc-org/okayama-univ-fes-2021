24e52c834cd8ee793f7c0fd44191cc193bfaa3af
sasigume <sasigume@gmail.com>
2021-10-05 22:43:54 +0900

    Fix: 影の部分がない画像に差し替え、CSSで影をつけた

--- pages/login.vue
+++ pages/login.vue
@@ -1,44 +1,42 @@
 <template>
-  <div
-    class="py-8 w-screen flex flex-col justify-center items-center"
-    v-if="!isLoading"
-  >
-    <a
-      id="button-login-google"
-      class="cursor-pointer inline-block mx-auto"
-      v-if="!isLoading"
-      @click="signInWithGoogleRedirect"
-    >
-      <img
-        src="@/assets/img/button_login_google.png"
-        width="206px"
-        height="55px"
-      />
-    </a>
+  <div v-if="!isLoading" class="py-8 w-screen flex flex-col justify-center items-center">
+    <button@click="signInWithGoogleRedirect" v-if="!isLoading" id="button-login-google" class="mx-auto" />
   </div>
 </template>
 
 <script>
-import firebase, { googleProvider } from "~/plugins/firebase";
+import firebase, { googleProvider } from '~/plugins/firebase'
 
 export default {
-  name: "Login",
-  middleware: "skipLogin",
-  data() {
+  name: 'Login',
+  middleware: 'skipLogin',
+  data () {
     return {
       isLoading: true
-    };
+    }
   },
-  mounted() {
-    this.isLoading = false;
+  mounted () {
+    this.isLoading = false
   },
   methods: {
-    signInWithGoogleRedirect() {
-      firebase.auth().signInWithRedirect(googleProvider);
-      this.isLoading = true;
+    signInWithGoogleRedirect () {
+      firebase.auth().signInWithRedirect(googleProvider)
+      this.isLoading = true
     }
   }
-};
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+#button-login-google {
+  background-image: url(@/assets/img/button_login_google_without_shadow.png);
+  width: 194px;
+  height: 43px;
+  background-size: 194px 43px;
+  @apply shadow-md transition-shadow duration-300 rounded-sm;
+}
+
+#button-login-google:hover {
+  @apply shadow-lg;
+}
+</style>
