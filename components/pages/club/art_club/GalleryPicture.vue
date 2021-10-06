26e44b05b6f361271c3049766332ccf19d74f720
AAAR-Salmon <tenzikujohn@gmail.com>
2021-10-06 11:58:40 +0900

    Add: 美術部のページを作成

---
+++ components/pages/club/art_club/GalleryPicture.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <img :src="source" :alt="title" class="max-h-screen">
+    <dl class="text-center">
+      <dt class="ms-mincho text-12pt">
+        {{ title }}
+      </dt>
+      <dd class="ms-mincho text-12pt">
+        {{ artist }}
+      </dd>
+      <dd class="ms-mincho text-10pt">
+        {{ medium }}
+      </dd>
+      <dd v-if="this.$slots.default" class="ms-mincho text-10pt border-t-2 border-black pt-1">
+        <slot />
+      </dd>
+    </dl>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    source: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    artist: {
+      type: String,
+      required: true
+    },
+    medium: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.ms-mincho {
+  font-family: 'MS 明朝', 'MS Mincho', 'Noto Serif JP', serif;
+}
+
+.text-12pt {
+  font-size: 1rem;
+}
+
+.text-10pt {
+  font-size: 0.8333rem;
+}
+</style>
---
+++ pages/clubs/art_club.vue
@@ -0,0 +1,212 @@
+<template>
+  <div class="container max-w-screen-xl mt-24 mx-auto relative">
+    <section>
+      <h1 class="text-center yu-mincho font-bold text-24pt leading-loose">
+        美術部<br>
+        オンライン美術展
+      </h1>
+      <p class="text-center yu-mincho font-bold text-18pt">
+        美術部では各部員が様々なジャンルの作品を制作しています。個性的な作品を是非お楽しみください。
+      </p>
+      <p class="text-center yu-mincho text-14pt mt-8">
+        当サイト内の作品の保存、スクリーンショット、二次利用(転載・コピー・引用)及び複製はお控えください。
+      </p>
+    </section>
+
+    <section class="mt-8">
+      <h2 class="ms-mincho text-22pt">
+        水彩画
+      </h2>
+      <p class="ms-mincho text-11-5pt">
+        水溶性の絵の具を使用して書いた絵のことです。<br>
+        淡い色合いや透明感が最大の魅力です。
+      </p>
+      <div>
+        <GalleryPicture
+          :source="require('~/assets/img/dev/hero.jpg')"
+          title="葵"
+          artist="赤瀬 幸香（工・3年）"
+          medium="透明水彩絵の具"
+        >
+          髪の毛を見てほしいです。
+        </GalleryPicture>
+        <GalleryPicture
+          :source="require('~/assets/img/dev/hero.jpg')"
+          title="退屈"
+          artist="赤瀬 幸香（工・3年）"
+          medium="透明水彩絵の具"
+        >
+          青、黄色系でまとめました。
+        </GalleryPicture>
+      </div>
+    </section>
+
+    <section class="mt-8">
+      <h2 class="ms-mincho text-22pt">
+        アクリル画
+      </h2>
+      <p class="ms-mincho text-11-5pt">
+        アクリル樹脂の絵の具を使っています。<br>
+        鮮やかな発色が特徴的です。
+      </p>
+      <GalleryPicture
+        :source="require('~/assets/img/dev/hero.jpg')"
+        title="金を継ぐ"
+        artist="高田 ひかる（文・3年）"
+        medium="キャンバスボード、アクリル絵の具"
+      >
+        傷の誇り
+      </GalleryPicture>
+    </section>
+
+    <section class="mt-8">
+      <h2 class="ms-mincho text-22pt">
+        油絵
+      </h2>
+      <p class="ms-mincho text-11-5pt">
+        つややかで透明感のある質感から重厚な雰囲気まで、表現の幅が限りなく広いところが油絵の魅力です。
+      </p>
+      <GalleryPicture
+        :source="require('~/assets/img/dev/hero.jpg')"
+        title="カーテンと少女"
+        artist="富松 莉子（文・1年）"
+        medium="油絵具"
+      >
+        カーテンの前に立つ少女を描きました。<br>
+        揺れやしわを表現するのが楽しかったです。
+      </GalleryPicture>
+    </section>
+
+    <section class="mt-8">
+      <h2 class="ms-mincho text-22pt">
+        色鉛筆画
+      </h2>
+      <p class="ms-mincho text-11-5pt">
+        塗り重ねることで独特の色合いを繊細に表現することができます。
+      </p>
+      <GalleryPicture
+        :source="require('~/assets/img/dev/hero.jpg')"
+        title="ルリコンゴウインコ"
+        artist="棚倉 淳郎（工・2年）"
+        medium="色鉛筆"
+      >
+        10色で描いています。
+      </GalleryPicture>
+      <GalleryPicture
+        :source="require('~/assets/img/dev/hero.jpg')"
+        title="オオセンチコガネ"
+        artist="棚倉 淳郎（工・2年）"
+        medium="色鉛筆"
+      >
+        躍動感を意識して描きました。
+      </GalleryPicture>
+    </section>
+
+    <section class="mt-8">
+      <h2 class="ms-mincho text-22pt">
+        水彩色鉛筆画
+      </h2>
+      <p class="ms-mincho text-11-5pt">
+        水彩色鉛筆は芯が水溶性の素材でできており、水を含ませると水彩画のような表現が可能です。
+      </p>
+      <GalleryPicture
+        :source="require('~/assets/img/dev/hero.jpg')"
+        title="待つ"
+        artist="高村 侑（工・2年）"
+        medium="ペン、水彩色鉛筆"
+      >
+        電話ボックスを描きました。
+      </GalleryPicture>
+      <GalleryPicture
+        :source="require('~/assets/img/dev/hero.jpg')"
+        title="帰る"
+        artist="高村 侑（工・2年）"
+        medium="ペン、水彩色鉛筆"
+      >
+        琵琶湖を描きました。
+      </GalleryPicture>
+    </section>
+
+    <section class="mt-8">
+      <h2 class="ms-mincho text-22pt">
+        立体作品
+      </h2>
+      <p class="ms-mincho text-11-5pt">
+        折り紙や、粘土、食品サンプルなど様々な種類があります。<br>
+        実際の質感をじかに感じられるところが立体作品の魅力です。
+      </p>
+      <GalleryPicture
+        :source="require('~/assets/img/dev/hero.jpg')"
+        title="アーモンド(偽物)"
+        artist="江尻 明日香（文・3年）"
+        medium="樹脂粘土、アクリルガッシュ、合成樹脂塗料"
+      />
+      <GalleryPicture
+        :source="require('~/assets/img/dev/hero.jpg')"
+        title="アーモンドのキャラメリゼ(偽物)"
+        artist="江尻 明日香（文・3年）"
+        medium="樹脂粘土、アクリルガッシュ、合成樹脂塗料"
+      />
+      <GalleryPicture
+        :source="require('~/assets/img/dev/hero.jpg')"
+        title="バター(偽物)"
+        artist="江尻 明日香（文・3年）"
+        medium="樹脂粘土、合成樹脂塗料"
+      />
+      <GalleryPicture
+        :source="require('~/assets/img/dev/hero.jpg')"
+        title="栗の甘露煮(偽物)"
+        artist="江尻 明日香（文・3年）"
+        medium="樹脂粘土、レジン液、アクリルガッシュ、合成樹脂塗料"
+      />
+      <GalleryPicture
+        :source="require('~/assets/img/dev/hero.jpg')"
+        title="煮物(偽物)"
+        artist="江尻 明日香（文・3年）"
+        medium="樹脂粘土、木粉粘土、レジン液、アクリルガッシュ"
+      />
+    </section>
+  </div>
+</template>
+
+<script>
+// noinspection NpmUsedModulesInstalled
+import GalleryPicture from '@/components/pages/club/art_club/GalleryPicture'
+
+// noinspection JSUnusedGlobalSymbols
+export default {
+  components: {
+    GalleryPicture
+  }
+}
+</script>
+
+<style scoped>
+.text-24pt {
+  font-size: 2rem;
+}
+
+.text-18pt {
+  font-size: 1.5rem;
+}
+
+.text-14pt {
+  font-size: 1.167rem;
+}
+
+.text-22pt {
+  font-size: 1.833rem;
+}
+
+.text-11-5pt {
+  font-size: 0.9583rem;
+}
+
+.yu-mincho {
+  font-family: '游明朝体', 'Yu Mincho', YuMincho, 'ヒラギノ明朝 Pro', 'Hiragino Mincho Pro', 'Noto Serif JP', 'MS P明朝', 'MS PMincho', serif;
+}
+
+.ms-mincho {
+  font-family: 'MS 明朝', 'MS Mincho', 'Noto Serif JP', serif;
+}
+</style>
