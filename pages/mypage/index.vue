4bf96c2c03b4b4056b1e1a5d088f2cc887ec69ff
AAAR-Salmon <tenzikujohn@gmail.com>
2021-10-11 03:14:02 +0900

    Clean: 景品のデータを.tsファイルにまとめた

--- pages/mypage/index.vue
+++ pages/mypage/index.vue
@@ -20,7 +20,7 @@
 
     <BodyWithHeader title="スタンプ達成状況" colors="border-themeColor text-themeColor">
       <div class="grid md:grid-cols-2 gap-4">
-        <!-- TODO: スタンプのアイコンと説明を設定（直で書くと鬱陶しいのでJSONかなんかにまとめてwebpackでimport）
+        <!-- TODO: スタンプのアイコンと説明を設定（直で書くと鬱陶しいので適当なファイルにまとめてimport）
                    取得状況は例によって vuex でよいでしょう -->
         <div
           v-for="i in Array(5).map((_, $i) => $i)"
@@ -40,7 +40,7 @@
     </BodyWithHeader>
 
     <BodyWithHeader title="プレゼント応募" colors="border-themeColor text-themeColor">
-      <StampRallyGrid :items="items" />
+      <StampRallyGrid :items="stampRallyItems" />
 
       <div class="mt-8 col-span-2 md:col-span-1 text-center md:text-left md:pl-3">
         <!-- TODO: 応募リンクに変更 -->
@@ -60,7 +60,7 @@ import Header from '~/components/layouts/Header.vue'
 import BodyWithHeader from '~/components/templates/header/BodyWithHeader.vue'
 import VerticalTitle from '~/components/layouts/VerticalTitle.vue'
 import StampRallyGrid from '~/components/pages/stamp-rally/Grid.vue'
-import { IStampRallyItem } from '~/assets/js/type/IStampRally'
+import stampRallyItems from '~/assets/data/stamp-rally-item'
 
 export default {
   components: {
@@ -70,32 +70,8 @@ export default {
     StampRallyGrid
   },
   data () {
-    const items: IStampRallyItem[] = [
-      {
-        id: 'nintendo-switch',
-        name: 'Nintendo Switch',
-        presentCount: 2,
-        stampCount: 10,
-        cover: {
-          url: 'https://images.microcms-assets.io/assets/7734751c51674cb2966df9edbbd3b15b/31f8c1358ce644cd96b24b41250848a8/%E3%83%A1%E3%82%A4%E3%83%B3.jpg',
-          height: 556,
-          width: 1280
-        }
-      },
-      {
-        id: 'ps5',
-        name: 'PS5',
-        presentCount: 1,
-        stampCount: 30,
-        cover: {
-          url: 'https://images.microcms-assets.io/assets/7734751c51674cb2966df9edbbd3b15b/31f8c1358ce644cd96b24b41250848a8/%E3%83%A1%E3%82%A4%E3%83%B3.jpg',
-          height: 556,
-          width: 1280
-        }
-      }
-    ]
     return {
-      items
+      stampRallyItems
     }
   }
 }
