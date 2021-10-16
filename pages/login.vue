d63c48c7dc3ead8fe3b3bd2236702b012481e516
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-10-16 15:44:44 +0900

    Google ログイン 追加

---
+++ pages/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>ログイン画面</h1>
+    <button @click="googleAuthenticate">
+      Google
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  methods: {
+    googleAuthenticate () {
+      this.$auth.loginWith('google')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
