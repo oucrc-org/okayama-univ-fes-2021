d44c6b13c1d9c001193a6881f7165d5b374f681d
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-10-17 22:23:49 +0900

    Fix: マイページ Rebase

--- pages/mypage/index.vue
+++ pages/mypage/index.vue
@@ -5,18 +5,19 @@
 
     <BodyWithHeader title="ユーザ情報" colors="border-themeColor text-themeColor">
       <div class="flex flex-row gap-x-4">
-        <!-- TODO: vuex state からユーザ情報を取得（feature/google-authのマージ後） -->
         <img
-          src="https://lh3.googleusercontent.com/ogw/ADea4I41QDHDR0OiOzULZnxwCOqNkGwgvL4m29vZ549f=s83-c-mo"
-          alt=""
+          :src="$store.state.auth.user.picture"
+          :alt="$store.state.auth.user.name"
           class="w-16 md:w-24 rounded-full"
         >
         <div class="justify-center flex flex-col">
-          <span class="text-gray-500 text-xs truncate">okadai-taro@example.com</span>
-          <span class="text-xl">岡大 太郎</span>
+          <span class="text-gray-500 text-xs truncate">{{ $store.state.auth.user.email }}</span>
+          <span class="text-xl">{{ $store.state.auth.user.name }}</span>
         </div>
       </div>
     </BodyWithHeader>
+
+    <span class="text-xl">AccessToken: {{ $auth.getToken('google') }}</span>
 
     <BodyWithHeader title="スタンプ達成状況" colors="border-themeColor text-themeColor">
       <div class="grid md:grid-cols-2 gap-4">
@@ -44,34 +45,36 @@
 
       <div class="mt-8 col-span-2 md:col-span-1 text-center md:text-left md:pl-3">
         <!-- TODO: 応募リンクに変更 -->
-        <NuxtLink
+        <LinkTo
           to="#"
           class="border-3 border-themeColor bg-themeColor inline-block font-medium text-center text-white text-lg tracking-wider rounded-full w-11/12 md:w-72 py-3 transform transition duration-300 hover:scale-105"
         >
           応募する →
-        </NuxtLink>
+        </LinkTo>
       </div>
     </BodyWithHeader>
 
     <div class="text-center my-8">
-      <NuxtLink to="#" class="text-gray-600 underline text-sm">
+      <LinkTo to="#" class="text-gray-600 underline text-sm">
         ログアウト
-      </NuxtLink>
+      </LinkTo>
     </div>
   </div>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import Header from '~/components/layouts/Header.vue'
 import BodyWithHeader from '~/components/templates/header/BodyWithHeader.vue'
 import VerticalTitle from '~/components/layouts/VerticalTitle.vue'
 import StampRallyGrid from '~/components/pages/stamp-rally/Grid.vue'
 import stampRallyItems from '~/assets/data/stamp-rally-item'
+import LinkTo from '~/components/templates/nuxt/LinkTo.vue'
 
 export default {
   components: {
     Header,
     BodyWithHeader,
+    LinkTo,
     VerticalTitle,
     StampRallyGrid
   },
