df247ef5cd450032cfae1bd12547f09e4513a25e
AAAR-Salmon <tenzikujohn@gmail.com>
2021-10-24 15:06:53 +0900

    Clean: リンク用角丸ボタンをコンポーネント化

---
+++ components/templates/parts/RoundedButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="border-3 border-themeColor bg-themeColor inline-block font-medium text-center text-white text-lg tracking-wider rounded-full w-11/12 md:w-72 py-3 transform transition duration-300 hover:scale-105"
+  >
+    {{ text }}
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
