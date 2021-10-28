e9b140ba6df3908ae08f4e52f5d448427f9d5244
gyarudev <sasigume@gmail.com>
2021-10-28 12:23:20 +0900

    Add: 日替わり動画コンポーネントを作成

---
+++ components/templates/video/DailyVideo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div v-if="url" class="relative overflow-hidden" style="padding-bottom: 56.25%">
+      <iframe
+        class="absolute h-full w-full"
+        :src="watchToEmbed(url)"
+        title="学祭2021 パフォーマンス"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      />
+    </div>
+    <div v-else>
+      動画が設定されていません
+    </div>
+    <div class="text-center font-bold py-4">
+      <div v-if="nextVideoDateString">
+        次の動画は{{ nextVideoDateString }}に公開!
+      </div>
+      <div v-else>
+        これで最後の動画です!
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import getTodayVideoUrl from '~/assets/js/video/get-today-video-url'
+import watchToEmbed from '~/assets/js/url/watch-to-embed'
+
+export default {
+  name: 'DailyVideo',
+  data () {
+    const { url, nextVideoDateString } = getTodayVideoUrl()
+    return { url, nextVideoDateString }
+  },
+  methods: {
+    watchToEmbed
+  }
+}
+</script>
+
+<style>
+</style>
