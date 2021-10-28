2c84fe65d357513eda79c219f1e6eae69fceeaa4
gyarudev <sasigume@gmail.com>
2021-10-28 14:40:37 +0900

    Add: フッターメニュー用コンポーネントを作成

---
+++ components/templates/parts/LinkWithArrow.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <LinkTo
+      :to="to"
+      class="flex justify-between text-base p-3"
+      :class="{active: $nuxt.$route.path === to }"
+    >
+      <b>{{ title }}</b>
+      <i><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+      </svg></i>
+    </LinkTo>
+  </div>
+</template>
+
+<script lang="ts">
+import LinkTo from '~/components/templates/nuxt/LinkTo.vue'
+export default {
+  name: 'LinkWithArror',
+  components: { LinkTo },
+  props: {
+    to: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
---
+++ components/templates/parts/SlideDown.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- daisyUIのCSSで動いているためtabindexが必要 -->
+  <!-- 詳細: https://daisyui.com/components/collapse -->
+  <div tabindex="0" class="collapse collapse-plus">
+    <div class="collapse-title p-3 items-center font-bold">
+      {{ label }}
+    </div>
+
+    <div class="collapse-content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'SlideDown',
+  props: {
+    label: { type: String, required: true }
+  }
+}
+</script>
+
+<style>
+/* daisyUIの縦幅を調整 */
+.collapse-title {
+  min-height: auto;
+}
+.collapse-content {
+  padding: 0px;
+}
+/* daisyUIのプラスマークを大きくする */
+.collapse-title::after {
+  font-weight: normal;
+  line-height: 1rem;
+  font-size: 2rem;
+}
+</style>
