438d1f750e5a76b410d78394f5d3e30e936578ba
gyarudev <sasigume@gmail.com>
2021-10-29 13:37:29 +0900

    Add: タイトルなどを追加、ボタンを共通化

--- components/templates/video/DailyLiveOrVideo.vue
+++ components/templates/video/DailyLiveOrVideo.vue
@@ -22,36 +22,54 @@
         allowfullscreen
       />
     </div>
+
+    <div class="flex flex-col gap-y-4 font-bold py-8">
+      <div v-if="title">
+        タイトル: {{ title }}
+      </div>
+      <div v-if="who">
+        出演: {{ who }}
+      </div>
+      <div v-if="timeFrame">
+        放送時間: {{ timeFrame }}
+      </div>
+      <div v-if="nextVideoDateString">
+        次の放送: {{ nextVideoDateString }}に公開!
+      </div>
+    </div>
     <div v-if="isLive">
       <client-only>
         <OperationStageSchedule class="max-w-screen-md mx-auto" />
       </client-only>
     </div>
-    <div v-else class="text-center font-bold py-4">
-      <div v-if="nextVideoDateString">
-        次の動画は{{ nextVideoDateString }}に公開!
-      </div>
-      <div v-else>
-        これで最後の動画です!
-      </div>
-    </div>
-
     <!-- TODO: ボタンデザイン -->
-    <a href="https://www.youtube.com/channel/UCxznqY3mWr_EEQ-FDXA0IEA/videos" target="_blank">過去の動画を見る</a>
+    <div class="text-center mt-8">
+      <RoundButton v-if="isLive" :href="url" target="_blank" colors="bg-red-500">
+        YouTubeでみる →
+      </RoundButton>
+      <RoundButton v-else href="https://www.youtube.com/channel/UCxznqY3mWr_EEQ-FDXA0IEA/videos" target="_blank">
+        過去の動画を見る →
+      </RoundButton>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+import RoundButton from '../parts/RoundButton.vue'
 import getTodayVideoUrl from '~/assets/js/video/get-today-video-url'
 import watchToEmbed from '~/assets/js/url/watch-to-embed'
 import Header from '~/components/layouts/Header.vue'
 import OperationStageSchedule from '~/components/pages/schedule/OperationStageSchedule.vue'
 
 export default {
-  components: { Header, OperationStageSchedule },
+  components: {
+    Header,
+    OperationStageSchedule,
+    RoundButton
+  },
   data () {
-    const { url, isLive, timeFrame, nextVideoDateString } = getTodayVideoUrl()
-    return { url, isLive, timeFrame, nextVideoDateString }
+    const { url, isLive, title, who, timeFrame, nextVideoDateString } = getTodayVideoUrl()
+    return { url, isLive, title, who, timeFrame, nextVideoDateString }
   },
   methods: {
     watchToEmbed
