3f10c4883980cc8b194857564aafd8d45a3e2e0b
gyarudev <sasigume@gmail.com>
2021-10-29 10:55:54 +0900

    Add: 同意以外の項目を追加

--- pages/quiz/form.vue
+++ pages/quiz/form.vue
@@ -6,12 +6,25 @@
       action="https://fb48-101-141-95-93.ngrok.io/api/present-form"
       method="post"
     >
-      <!--suppress JSUnresolvedVariable -->
+      <!--suppress JSUnresolvedVariable  -->
       <input type="hidden" name="google_id" :value="$auth.getToken('google')">
+      <!--suppress JSUnresolvedVariable  -->
+      <input type="hidden" name="email" :value="$auth.email">
 
-      <TextInput id="name" label="名前" name="name" placeholder="例:岡大太郎" />
+      <div class="grid grid-cols-2 gap-3">
+        <TextInput name="family_name" autocomplete="family-name" label="姓" placeholder="" />
+        <TextInput name="given_name" autocomplete="given-name" label="名" placeholder="" />
+      </div>
+      <div class="grid grid-cols-2 gap-3">
+        <TextInput name="family_name_kana" label="セイ" placeholder="" />
+        <TextInput name="given_name_kana" label="メイ" placeholder="" />
+      </div>
 
-      <TextInput id="address" label="住所" name="address" placeholder="例:岡山市北区〇〇" />
+      <TextInput name="email" type="email" autocomplete="email" label="予備のメールアドレス" placeholder="例: XXXX@yahoo.co.jp" />
+      <TextInput name="tel" autocomplete="tel-national" type="tel" label="お電話番号" />
+
+      <TextInput name="postal_code" autocomplete="postal-code" label="郵便番号" placeholder="例: 7008530" />
+      <TextInput name="address" autocomplete="street-address" label="住所" placeholder="例: 岡山市北区〇〇" />
 
       <SelectInput id="presents" label="応募するプレゼント" :options="presents" :stamp-number="stampNumber" />
 
@@ -27,7 +40,7 @@ import Header from '~/components/layouts/Header.vue'
 import SubmitButton from '~/components/templates/form/SubmitButton.vue'
 import TextInput from '~/components/templates/form/TextInput.vue'
 import SelectInput from '~/components/templates/form/RadioInput.vue'
-import presentFormData from "~/assets/data/present-form-data";
+import presentFormData from '~/assets/data/present-form-data'
 
 const baseUrl = 'https://35cc-101-141-95-93.ngrok.io'
 
