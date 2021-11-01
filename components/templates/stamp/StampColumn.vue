8092cf211100bca90aced39275813c49f4e13152
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-11-01 13:47:22 +0900

    [add] 取得したスタンプを表示する機能を追加

---
+++ components/templates/stamp/StampColumn.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex items-center">
+    <div class="avatar">
+      <div class="my-5 rounded-full w-24 h-24">
+        <img :src="src" :alt="alt">
+      </div>
+    </div>
+    <div class="ml-8 text-base font-medium">
+      {{ name }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  props: {
+    src: {
+      type: String,
+      default: ''
+    },
+    alt: {
+      type: String,
+      default: ''
+    },
+    name: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
---
+++ components/templates/stamp/StampStatus.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="card shadow-2xl lg:card-side">
+      <div class="card-body inline-block">
+        <div v-for="stamp in stamps" :key="stamp.id">
+          <StampColumn
+            :src="stamp.has_stamp ? stamp.image_path : require('@/assets/img/icon/grey-icon.jpg')"
+            :alt="stamp.name"
+            :name="stamp.has_stamp ? stamp.name : '????'"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import StampColumn from '~/components/templates/stamp/StampColumn.vue'
+
+export default {
+  components: { StampColumn },
+  props: {
+    stamps: {
+      type: Array,
+      // eslint-disable-next-line vue/require-valid-default-prop
+      default: []
+    }
+  }
+}
+</script>
