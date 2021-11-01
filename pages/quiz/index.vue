8bac6e986505bd525bed62a9d57952ad97856961
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-11-01 20:30:48 +0900

    Update: 今日のクイズ 応募期間表示

--- pages/quiz/index.vue
+++ pages/quiz/index.vue
@@ -88,10 +88,29 @@
 
     <section>
       <BodyWithHeader title="応募ページ" colors="border-themeColor text-themeColor">
-        <p class="mb-8">
-          最終日以降、貯めたスタンプ数に応じて景品に応募することができます。
-        </p>
-        <div class="col-span-2 md:col-span-1 text-center md:text-left md:pl-3">
+        <div class="alert">
+          <div class="flex-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="#0071C5"
+              class="flex-shrink-0 w-6 h-6 mx-2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+              />
+            </svg>
+            <label>
+              <h4>プレゼント応募は、<b>11月18日</b>（学祭終了後）より開始します。</h4>
+            </label>
+          </div>
+        </div>
+
+        <div class="text-center mt-12 md:pl-3">
           <RoundedButton text="応募する →" disabled="true" class="border-blue-300 bg-blue-300 text-white" />
         </div>
       </BodyWithHeader>
