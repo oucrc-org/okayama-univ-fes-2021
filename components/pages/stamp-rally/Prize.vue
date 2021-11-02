4dd90bec12186b6844548537901fe72d2625f7cb
AAAR-Salmon <tenzikujohn@gmail.com>
2021-11-03 01:01:50 +0900

    Add: グレーアウトの実装
    文字は大変だったので諦めました

--- components/pages/stamp-rally/Prize.vue
+++ components/pages/stamp-rally/Prize.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="relative flex flex-col gap-y-2">
-    <picture>
-      <source :srcset="`${item.image_path}.webp`" type="image/webp">
-      <img :src="`${item.image_path}.jpg`" :alt="item.name" class="object-cover rounded-lg bg-gray-500 h-40">
-    </picture>
+    <div class="bg-disabled-picture rounded-lg" :data-disabled="disabled">
+      <picture>
+        <source :srcset="`${item.image_path}.webp`" type="image/webp">
+        <img :src="`${item.image_path}.jpg`" :alt="item.name" class="object-cover rounded-lg h-40" :style="disabled ? { filter: 'opacity(0.4)' } : {}">
+      </picture>
+    </div>
 
-    <div class="text-xl font-bold w-10/12 sm:w-auto">
+    <div class="text-xl font-bold w-10/12 sm:w-auto" :class="disabled ? ['text-gray-400'] : []">
       {{ item.name }}
     </div>
     <div class="text-gray-700">
       抽選{{ item.stock }}名
     </div>
-    <div class="absolute top-4 left-44 rounded-sm text-white text-xs bg-themeColor px-3 py-2">
-      スタンプ{{ item.required_stamps }}コ
+    <div class="bg-disabled absolute top-4 left-44 rounded-sm" :data-disabled="disabled">
+      <div class="rounded-sm text-white text-xs bg-themeColor px-3 py-2" :style="disabled ? { filter: 'opacity(0.4)' } : {}">
+        スタンプ{{ item.required_stamps }}コ
+      </div>
     </div>
   </div>
 </template>
+
 <script lang="ts">
 // eslint-disable-next-line import/named
 import Vue, { PropType } from 'vue'
--- pages/mypage/index.vue
+++ pages/mypage/index.vue
@@ -43,7 +43,7 @@
     <section>
       <BodyWithHeader title="豪華景品" colors="border-themeColor text-themeColor">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <StampRallyPrize v-for="item in prizes" :key="item.id" :item="item" />
+          <StampRallyPrize v-for="item in prizes" :key="item.id" :item="item" :disabled="item.required_stamps > stampCount" />
         </div>
       </BodyWithHeader>
     </section>
