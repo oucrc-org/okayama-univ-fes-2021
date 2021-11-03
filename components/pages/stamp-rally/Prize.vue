5a4414fa38a3d2c9435151c04eeab4a890ac4ed9
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-11-03 13:32:32 +0900

    Update: スタンプ不足 バッジ追加

--- components/pages/stamp-rally/Prize.vue
+++ components/pages/stamp-rally/Prize.vue
@@ -5,6 +5,9 @@
         <source :srcset="`${item.image_path}.webp`" type="image/webp">
         <img :src="`${item.image_path}.jpg`" :alt="item.name" class="object-cover rounded-lg h-40" :style="disabled ? { filter: 'opacity(0.4)' } : {}">
       </picture>
+      <div v-if="disabled" class="absolute top-5 left-2 badge bg-red-500 border-red-500 ml-2 inline-block">
+        <span class="px-1 inline-block">スタンプ不足</span>
+      </div>
     </div>
 
     <div class="text-xl font-bold w-10/12 sm:w-auto" :class="disabled ? ['text-gray-400'] : []">
@@ -14,7 +17,7 @@
       抽選{{ item.stock }}名
     </div>
     <div class="bg-disabled absolute top-4 left-44 rounded-sm" :data-disabled="disabled">
-      <div class="rounded-sm text-white text-xs bg-themeColor px-3 py-2" :style="disabled ? { filter: 'opacity(0.4)' } : {}">
+      <div class="rounded-sm text-white text-xs bg-themeColor px-3 py-2">
         スタンプ{{ item.required_stamps }}コ
       </div>
     </div>
