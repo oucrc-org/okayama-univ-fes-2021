4c43c9e9e89056394da4cb8f757430365fc2b956
massan1111 <t.hiro1182@gmail.com>
2021-11-03 13:57:27 +0900

    Add:Google認証中(Callback)画面の実装とそれに係る画像の追加

--- pages/callback.vue
+++ pages/callback.vue
@@ -1,16 +1,29 @@
 <template>
-  <div>
-    <h1>callback</h1>
-    <p>少々お待ち下さい</p>
+  <div class="container  max-w-screen-lg mx-auto px-3 py-8">
+    <VerticalTitle text="CALLBACK" colors="text-gray-200" class="-z-10" />
+    <Header title="読み込み中" colors="bg-themeColor text-white" />
+    <div class="flex flex-col gap-y-16 my-16">
+      <div class="mx-auto">
+        <img src="../assets/img/static/redirecting.png" width="90" />
+      </div>
+      <p class=" text-themeColor text-2xl text-center font-bold">
+        少々お待ちください
+      </p>
+      <p class="text-center my-15">
+        Google認証の応答を待っています。数分経っても画面が遷移しない場合は、再読み込みしてください。
+      </p>
+    </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import Header from '~/components/layouts/Header.vue'
+import VerticalTitle from '~/components/layouts/VerticalTitle.vue'
+
 export default {
-  name: 'Callback'
+  name: 'Callback',
+  components: { Header, VerticalTitle }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style></style>
