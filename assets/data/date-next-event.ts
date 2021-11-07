8c1af1e3be1f4c2a2488e3b6bcd0a372fe31f5d4
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-11-07 23:35:21 +0900

    Add: 次のイベント 修正

--- assets/data/date-next-event.ts
+++ assets/data/date-next-event.ts
@@ -20,9 +20,9 @@ interface EventData {
  * @see https://github.com/oucrc-org/okayama-univ-fes-2021/blob/main/assets/data/date-youtube-url.ts
  */
 const dateNextEvent: Record<string, EventData> = {
-  '2021-11-8 19:00': { time: '11/7（日）16:00 〜', name: 'プロライブ（ハナコ、四千頭身、ぼる塾）', webp: 'burner_next_event_procon.webp', jpg: 'burner_next_event_procon.jpg' },
+  '2021-11-1 00:00': { time: '11/7（日）16:00 〜', name: 'プロライブ（ハナコ、四千頭身、ぼる塾）', webp: 'burner_next_event_procon.webp', jpg: 'burner_next_event_procon.jpg' },
   '2021-11-16 21:00': { time: '11/16（火）18:00〜', name: '校友会ステージ企画（1日目)', webp: 'burner_next_event_stage.webp', jpg: 'burner_next_event_stage.jpg' },
-  '2021-11-17 21:00': { time: '11/17（水）18:00〜', name: '校友会ステージ企画（2日目）', webp: 'burner_next_event_stage.webp', jpg: 'burner_next_event_stage.jpg' }
+  '2021-11-18 00:00': { time: '11/17（水）18:00〜', name: '校友会ステージ企画（2日目）', webp: 'burner_next_event_stage.webp', jpg: 'burner_next_event_stage.jpg' }
 }
 
 export default dateNextEvent
