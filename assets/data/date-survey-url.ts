88d0b8c7d04d9337b4e78c5d090dbd77083a7eba
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-11-07 18:24:10 +0900

    Fix: アンケート期間変更

--- assets/data/date-survey-url.ts
+++ assets/data/date-survey-url.ts
@@ -7,7 +7,7 @@
  */
 const dateSurveyUrl: Record<string, string> = {
   '2021-10-30 00:00': 'https://forms.gle/BprtZhN6GWFo42L57',
-  '2021-11-8 00:00': 'https://forms.gle/bJKr4Ube3YVJkvRW8',
+  '2021-11-8 09:00': 'https://forms.gle/bJKr4Ube3YVJkvRW8',
   '2021-11-16 00:00': 'https://forms.gle/A4sp8cAFXwKkWFxx5',
   '2021-11-18 00:00': 'https://forms.gle/DvEy927YYvKypoxC6'
 }
