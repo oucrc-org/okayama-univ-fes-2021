713fabb49f31247afe6d8206fac2b44a8166dba0
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-11-07 18:46:07 +0900

    Add: アンケートバーナー表示

--- pages/index.vue
+++ pages/index.vue
@@ -188,6 +188,29 @@
 
     <NextEvent />
 
+    <div class="mx-auto max-w-screen-lg mt-12 px-5">
+      <div class="alert alert-info">
+        <div class="flex-1">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-6 h-6 mx-2 stroke-current">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <label>プロコンのご視聴ありがとうございました！岡大生へのフィードバックのため、アンケートにご協力ください！</label>
+        </div>
+        <div class="flex-none">
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSf_BhFm-OHc5bQPONfTCsFp4oQ__JhZoTiOuPa6V3cPLAKfcQ/viewform?usp=send_form"
+            class="btn btn-sm bg-themeColor border-themeColor hover:bg-themeLight hover:border-themeLight"
+            target="_blank"
+          >回答</a>
+        </div>
+      </div>
+    </div>
+
     <!-- ▼ バーナー -->
     <div class="grid md:grid-cols-2 gap-6 max-w-screen-lg mt-12 mx-auto px-8">
       <a href="/president-message">
