a753a51be2e6c0fdcfc30426ef0417dae786ab0b
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-11-07 17:42:23 +0900

    Remove: 配信Alert

--- components/templates/video/DailyLiveOrVideo.vue
+++ components/templates/video/DailyLiveOrVideo.vue
@@ -25,19 +25,6 @@
 
     <div class="mt-6 text-left flex-col lg:flex-row-reverse">
       <div>
-        <div class="alert alert-error mt-8">
-          <div class="flex-1">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-6 h-6 mx-2 stroke-current">
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              ></path>
-            </svg>
-            <label>現在配信に問題が発生しています。詳しくは学祭実行委員会の公式Twitterをご確認ください</label>
-          </div>
-        </div>
         <h1 v-if="title" class="mb-5 text-4xl font-bold mt-20" :class="isLive ? 'text-red-500' : 'text-themeColor'">
           {{ title }}
         </h1>
--- pages/live/index.vue
+++ pages/live/index.vue
@@ -10,19 +10,6 @@
         src="https://www.youtube-nocookie.com/embed/gwjxlonn43o"
         title="プロライブ"
       />
-      <div class="alert alert-error mt-6">
-        <div class="flex-1">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-6 h-6 mx-2 stroke-current">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            ></path>
-          </svg>
-          <label>現在配信に問題が発生しています。詳しくは学祭実行委員会の公式Twitterをご確認ください</label>
-        </div>
-      </div>
       <client-only>
         <div class="my-5">
           <ProConcertSchedule />
