a515ab668d8061d0becef8f0d13fbbf55fa25845
shu00011 <koha.koishi.gnu.16@gmail.com>
2021-11-16 20:10:08 +0900

    Add: 1日目アンケート お知らせ追加

--- pages/index.vue
+++ pages/index.vue
@@ -241,34 +241,34 @@
 
     <section class="w-full">
       <div class="relative items-center w-full px-8 py-3 mx-auto lg:py-6 max-w-screen-lg">
-        <!--<div class="alert alert-info mb-5">-->
-        <!--  <div class="flex-1">-->
-        <!--    <svg-->
-        <!--      xmlns="http://www.w3.org/2000/svg"-->
-        <!--      fill="none"-->
-        <!--      viewBox="0 0 24 24"-->
-        <!--      stroke="#0071C5"-->
-        <!--      class="flex-shrink-0 w-6 h-6 mx-2"-->
-        <!--    >-->
-        <!--      <path-->
-        <!--        stroke-linecap="round"-->
-        <!--        stroke-linejoin="round"-->
-        <!--        stroke-width="2"-->
-        <!--        d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"-->
-        <!--      ></path>-->
-        <!--    </svg>-->
-        <!--    <label class="mx-3">ステージ企画（1日目）のご視聴ありがとうございました。来年度の学祭のため、アンケートにご協力ください！</label>-->
-        <!--  </div>-->
-        <!--  <div class="flex-none">-->
-        <!--    <a-->
-        <!--      href="https://forms.gle/A4sp8cAFXwKkWFxx5"-->
-        <!--      target="_blank"-->
-        <!--      class="btn btn-sm btn-primary bg-themeColor border-themeColor hover:bg-themeLight hover:border-themeLight"-->
-        <!--    >-->
-        <!--      回答-->
-        <!--    </a>-->
-        <!--  </div>-->
-        <!--</div>-->
+        <div class="alert alert-info mb-5">
+          <div class="flex-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="#0071C5"
+              class="flex-shrink-0 w-6 h-6 mx-2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+              ></path>
+            </svg>
+            <label class="mx-3">ステージ企画（1日目）のご視聴ありがとうございました。来年度の学祭のため、アンケートにご協力ください！</label>
+          </div>
+          <div class="flex-none">
+            <a
+              href="https://forms.gle/A4sp8cAFXwKkWFxx5"
+              target="_blank"
+              class="btn btn-sm btn-primary bg-themeColor border-themeColor hover:bg-themeLight hover:border-themeLight"
+            >
+              回答
+            </a>
+          </div>
+        </div>
 
         <div class="alert">
           <div class="flex-1">
