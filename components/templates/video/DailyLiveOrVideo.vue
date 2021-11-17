17bd81ad9ecab45d0dbd07247b65f923d62df774
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-11-17 19:14:00 +0900

    Update: 注意書き

--- components/templates/video/DailyLiveOrVideo.vue
+++ components/templates/video/DailyLiveOrVideo.vue
@@ -40,6 +40,28 @@
       </div>
     </div>
 
+    <div class="alert mb-4 mt-8">
+      <div class="flex-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="#2196f3"
+          class="flex-shrink-0 w-6 h-6 mx-2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <label>
+          タイムスケジュールは変更されることがあります。再読み込みで最新のタイムスケジュールがご覧いただけます。
+        </label>
+      </div>
+    </div>
+
     <div v-if="isLive">
       <client-only>
         <OperationStageSchedule class="max-w-screen-md mx-auto" />
